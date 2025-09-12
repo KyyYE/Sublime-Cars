6379d42beb95eacfa02395cee286781a66b3cf97
Kyle <kylekxrr@pm.me>
2025-09-11 23:44:44 -0700

    all other shit because fuck this 2gb limit github are cucks

---
+++ [packs]/BCSO/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Nihan Development'
+description 'BCSO Pack V2 By Nihan Development'
+version '2.0.0'
+
+lua54 'yes'
+ 
+files {
+    'data/**/*.meta'
+}
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+dependency '/assetpacks'
